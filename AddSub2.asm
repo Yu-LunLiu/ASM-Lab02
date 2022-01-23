11ab90224b9f7d2bd3c6b42b5098c5aef66fe474
kevparrot.setp08@nctu.edu.tw <kevparrot.setp08@nctu.edu.tw>
2022-01-22 17:49:48 -0800

    a little changes. not big deal

--- AddSub2.asm
+++ AddSub2.asm
@@ -7,16 +7,18 @@ INCLUDE Irvine32.inc
 
 .data
 val1     dword  10000h
-val2     dword  40000h
-val3     dword  20000h
+val2     dword  20000h
+val3     dword  30000h
+val4     dword  40000h
 finalVal dword  ?
 
 .code
 main PROC
 
 	mov	eax,val1			; start with 10000h
-	add	eax,val2			; add 40000h
-	sub	eax,val3			; subtract 20000h
+	sub	eax,val2			; subtract 20000h
+	sub	eax,val3			; subtract 30000h
+	sub	eax,val4			; subtract 40000h
 	mov	finalVal,eax		; store the result (30000h)
 	call	DumpRegs			; display the registers
 
